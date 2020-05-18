c441616f78d4174bb2a184cc1a6e7d26e4fa4370
Alexander Reyes Rodriguez <arayrods@gmail.com>
2020-05-18 04:08:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,28 +21,26 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-    holiday_hash[:winter].each do |holiday, decorations|
+  holiday_hash[:winter].each do |holiday, decorations|
     decorations << supply
-   end
+  end
 end
 
-
 def add_supply_to_memorial_day(holiday_hash, supply)
-    holiday_hash[:spring][:memorial_day] << supply
-
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-    holiday_hash[season][holiday_name] = supply_array
-    holiday_hash
-
+  
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-     array = holiday_hash[:winter].map do |holiday, supply|
-     supply
-     end
-     array.flatten
+  array = holiday_hash[:winter].map do |holiday, supply|
+  supply
+end
+array.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -61,9 +59,6 @@ def all_holidays_with_bbq(holiday_hash)
     end
   end.flatten.compact
 end
-
-
-
 
 
 
